6c1c9df216e9f289956769cabf1d20d92569fc9f
mata <mata@matas-MacBook-Air.local>
2021-09-21 03:20:35 +0200

    redux fetchapi and cryptocurrencies created list filter and search

--- src/services/cryptoApi.js
+++ src/services/cryptoApi.js
@@ -1,8 +1,24 @@
-// var options = {
-//     method: 'GET',
-//     url: 'https://coinranking1.p.rapidapi.com/exchanges',
-//     headers: {
-//       'x-rapidapi-host': 'coinranking1.p.rapidapi.com',
-//       'x-rapidapi-key': 'b1b3051765msh91cdea31d8cdde3p1fd01fjsn9e846f5e9525'
-//     }
-//   };
+import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react';
+
+const cryptoApiHeaders = {
+  'x-rapidapi-host': 'coinranking1.p.rapidapi.com',
+  'x-rapidapi-key': '9d3b65f346msh67dba35862ca6f4p1644f0jsn42fec76a3697'
+}
+
+const baseUrl = 'https://coinranking1.p.rapidapi.com'
+
+const createRequest = (url) => ({ url, headers: cryptoApiHeaders })
+
+export const cryptoApi = createApi({
+  reducerPath: 'cryptoApi',
+  baseQuery: fetchBaseQuery({ baseUrl}),
+  endpoints: (builder) => ({
+    getCryptos: builder.query({
+      query: (count) => createRequest(`/coins?limit=${count}`),
+    }),
+  })
+});
+
+export const {
+  useGetCryptosQuery,
+} = cryptoApi;
